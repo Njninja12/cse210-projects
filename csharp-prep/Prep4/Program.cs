0ba947d3b4f1ac971864d671a76758727f2a7fa5
Njninja12 <noah.j.wright316@gmail.com>
2023-09-22 16:14:15 -0700

    completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int sum = 0;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers, type zero when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+            
+        }
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum = sum + numbers[i];
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+        int avg = sum / numbers.Count;
+        Console.WriteLine("Sum = " + sum);
+        Console.WriteLine("Average = " + avg);
+        Console.WriteLine("The largest number is: " + largest);
     }
 }
