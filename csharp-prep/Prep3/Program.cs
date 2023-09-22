fc600e9516f96c47253b74d558da762ec304e7c6
Njninja12 <noah.j.wright316@gmail.com>
2023-09-21 23:13:49 -0700

    completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1, 101);
+        
+
+        int guess_num = -1;
+
+        while (magic_num != guess_num)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess_num = int.Parse(Console.ReadLine());
+
+            if (guess_num > magic_num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess_num < magic_num)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed right!");
+            }
+        }
     }
 }
